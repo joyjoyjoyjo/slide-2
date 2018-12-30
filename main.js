cecbcc2700ffa7ce308d7a4c45a1364143dfffdc
xiaoyiyi <xiaoyishikongde@163.com>
2018-12-30 21:47:46 +0800

    添加轮播效果

---
+++ main.js
@@ -0,0 +1,42 @@
+let n
+init()
+setInterval(()=>{
+    makeLeave(getImage(n))
+    .one('transitionend',(e)=>{
+        makeEnter($(e.currentTarget))
+    })
+    makeCurrent(getImage(n+1))
+    n +=1
+},3000)
+
+
+
+
+function getImage(n){
+    return $(`.images > img:nth-child(${x(n)})`)
+  }
+function x(n){
+    if(n>5){
+        n = n%5
+        if(n===0){
+            n = 5
+        }
+    }
+    return n
+}
+
+function init(){
+    n = 1
+    $(`.images > img:nth-child(${n})`).addClass('current')
+    .siblings().addClass('enter')
+  }
+function makeCurrent($node){
+    $node.removeClass('enter leave').addClass('current')
+}
+function makeLeave($node){
+    $node.removeClass('enter current').addClass('leave')
+    return $node
+}
+function makeEnter($node){
+    $node.removeClass('leave current').addClass('enter')
+}
